c37f6d78ae92b19fcf3e1273f4f1db20b7496720
gabrieloftas <gabrieloftas@outlook.com>
2025-04-01 19:24:31 -0300

    Reset da branch

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        String x = "DEV";
-        if (x.equalsIgnoreCase("dev")) {
-            System.out.println("Testing Dev Enviroment");
-        }
-        else if (x.equalsIgnoreCase("Test")) {
-            System.out.println("Testing Enviroment");
-        }
+       
     }
 }
