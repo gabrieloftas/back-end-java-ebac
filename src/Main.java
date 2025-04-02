053fdcce2ba49c0e585d6a6b55fc7b1cb4e57da3
gabrieloftas <gabrieloftas@outlook.com>
2025-04-02 20:27:37 -0300

    Adicao de exercicio de controle de fluxo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        float[] notas = new float[4];
+        // Entrada das notas 
+        for (int i = 0; i < notas.length; i++) {
+            notas[i] = scanner.nextFloat();
+        }
+        // Cálculo da média
+        float media = 0;
+        for (float n : notas) {
+            media += n;
+        }
+        media = media/notas.length;
+        if (media >= 7) {
+            System.out.println("Aprovado");
+        }
+        else if (media >= 5) {
+            System.out.println("Recuperação");
+        }
+        else {
+            System.out.println("Reprovado");
+        }
     }
 }
