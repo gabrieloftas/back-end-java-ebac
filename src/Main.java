49f1f7df9198b7ebc8a4afa84a574907e49800a9
gabrieloftas <gabrieloftas@outlook.com>
2025-04-02 18:01:10 -0300

    Adicao do exercicio de casting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-       
+        char letra = 'G';
+        Character letraW = letra;
+        System.out.println("Letra : " + letraW);
     }
 }
