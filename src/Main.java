1c0c13ab6592389a99b00857f9af404b0b38ffbc
gabrieloftas <gabrieloftas@outlook.com>
2025-04-02 18:06:40 -0300

    reset da branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        char letra = 'G';
-        Character letraW = letra;
-        System.out.println("Letra : " + letraW);
+        
     }
 }
