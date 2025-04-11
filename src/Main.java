e86d99bcad6c547636d956f9d0badb7eaba85d95
Gabriel Oliveira de Freitas <gabrieloftas@outlook.com>
2025-04-10 23:15:47 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Main {
+    public static List<Pessoas> pessoasDoSexoFeminino = new ArrayList<>();
     public static void main(String[] args) {
-        
-        
-    
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> entrada = new ArrayList<>();
+        List<Pessoas> pessoas = new ArrayList<>();
+
+        entrada = Arrays.asList(scanner.next().split(","));
+
+        for (int i = 0; i < entrada.size() ; i=i+2) {
+            Pessoas p = new Pessoas(entrada.get(i), entrada.get(i+1));
+            pessoas.add(p);
+        }
+
+        System.out.println(pessoas);
+
+        //Stream pessoasPorSexo = pessoas.stream();
+
+        Ifiltro filtroPorSexo = ()->{
+            return pessoas.stream()
+                    .filter(p -> p.getSexo().equals("Feminino"))
+                    .toList();
+        };
+
+        pessoasDoSexoFeminino = filtroPorSexo.filtro();
+
+        System.out.println(pessoasDoSexoFeminino);
+
+        scanner.close();
     }
-    
+
 } 
 
 
